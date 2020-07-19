1e17668ef8d753bfd8ef714ae96889fc35b80401
Anthony Ledbetter <11atledbet@gmail.com>
2020-07-19 02:49:12 -0500

    The code now works more dynamically based off of a provided IP, then returns an output dictionary of the format
    Index, [date, time, messageType, host, length], [message]

--- HostMessagePuller.py
+++ HostMessagePuller.py
@@ -3,7 +3,40 @@ import paramiko
 # list of strings to look for in lines indicating that the line may be ignored.
 ccl_blacklist = ['SC Message Type', 'ERR:', '(2)']
 
-def main():
+def testIP(IP):
+
+    # Opens a new paramiko client that we then use to ssh with later.
+    client = paramiko.SSHClient()
+
+    # Tries to load any available system host keys.
+    client.load_system_host_keys()
+
+    # Overrides the host key if it is missing from the local system host keys.
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Try to connect to the requested machine
+    try:
+
+        # Connects to the host machine
+        client.connect(str(IP), 22, 'root', 'fiscal')
+
+        # Closes the client
+        client.close()
+
+        # Return true because we were able to connect to the host machine
+        return True
+
+    # Failed to connect to the requested machine
+    except:
+
+        # Close the client
+        client.close()
+
+        # Return false because we were unable to connect to the host machine
+        return False
+
+
+def pullMainCCLLog(IP):
 
     # Opens a new paramiko client that we then use to ssh with later.
     client = paramiko.SSHClient()
@@ -15,7 +48,7 @@ def main():
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     # Connects to the host machine
-    client.connect('192.168.101.133', 22, 'root', 'fiscal')
+    client.connect(str(IP), 22, 'root', 'fiscal')
 
     # Executes commands across to the connected client.
     # The commands executed navigate to the ccl directory and less the CCLlog.
@@ -27,8 +60,19 @@ def main():
     # Here we close the ssh connection to the machine.
     client.close()
 
+    # Return the gathered lines
+    return lines
+
+def pullCCLlogDate(IP, date):
+    print("moo")
+
+def logFilter(lines):
+
     # Initialize the output variable(filteredCCLlog) used to hold important data.
     filteredCCLlog = []
+
+    # Build a dictionary for holding all the messages produced by this tool.
+    outputDict = {}
 
     # For each line found in the standard output,
     for line in lines:
@@ -93,9 +137,53 @@ def main():
             # Append the message length to the relevant info array.
             relevantInfo.append(''.join(filter(lambda x: x.isdigit(), currentString[1])))
 
-            # Print the relevant information.
-            print('Index: ' + str(i) + ' - ' + str(relevantInfo))
+            # Initialize msgArray and make sure it is empty.
+            msgArray = []
 
+            # Initialize count for the for loop at 1
+            count = 1
+
+            # While message list length is less than the specified message length
+            while (len(msgArray) < int(relevantInfo[4])):
+
+                # Split the next index at 'INF: ' on the line starting at the index(i) plus the loop count
+                message = filteredCCLlog[i + count].split('INF: ')
+
+                # Split the message at the space characters and add them to the array.
+                message = message[1].split()
+
+                # Add all of the message bytes using '.extend'
+                msgArray.extend(message)
+
+                # Increment the counter - this is used to choose the line in the CCL log being operated on.
+                count += 1
+
+            # Get the length of the outputDict for use as the key/index
+            outputLen = len(outputDict)
+
+            # Create an entry in the dictionary for the new entry
+            outputDict[outputLen] = [relevantInfo, msgArray]
+
+    # Return the output dictionary
+    return outputDict
+
+def main():
+
+    IP = "192.168.101.133"
+
+    if testIP(IP):
+
+        lines = pullMainCCLLog(IP)
+
+        test = logFilter(lines)
+
+        print(str(len(test)))
+
+        for key, value in test.items():
+            print(str(key) + ' - ' + str(value))
+            print(str(key) + ' - ' + str(value[0][0]) + ' ' + str(value[0][1]))
+    else:
+        print("Failed to connect to the IP: " + str(IP))
 
 if __name__ == "__main__":
     main()
